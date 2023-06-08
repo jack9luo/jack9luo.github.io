0f94bdb58a871d37634940587e9366f2af1658c8
jack9luo <luoxiaohei0412@163.com>
2023-06-08 10:17:37 +0800

    Site updated: 2023-06-08 10:17:36

--- js/fomal.js
+++ js/fomal.js
@@ -2986,7 +2986,7 @@ function setUniverse() {
 
 // 雪花开关
 if (localStorage.getItem("snow") == undefined) {
-  localStorage.setItem("snow", "none");
+  localStorage.setItem("snow", "block");
 }
 document.getElementById("snow").style.display = localStorage.getItem("snow");
 function setSnow() {
